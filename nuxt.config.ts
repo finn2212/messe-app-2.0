799e505eec19011234f13a16f678d80335c3dbfe
finn2212 <stolle.finn@googlemail.com>
2025-03-10 12:03:01 +0200

    feat: update route

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      SUPABASE_URL: process.env.SUPABASE_URL,
+      SUPABASE_ANON_KEY: process.env.SUPABASE_ANON_KEY
+    }
+  },
   modules: [
     "nuxt-vuefire",
     "@nuxtjs/tailwindcss",
